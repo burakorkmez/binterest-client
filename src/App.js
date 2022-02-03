731c20c4e0fcde8552f0b72f3025a592971ece1a
Burak Örkmez <burakorkmezz@gmail.com>
2022-02-03 12:05:17 +0300

    check if user signed in

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './container/Home';
 
 const App = () => {
+	const navigate = useNavigate();
+
+	// check if user signed in
+	useEffect(() => {
+		const User =
+			localStorage.getItem('user') !== 'undefined'
+				? JSON.parse(localStorage.getItem('user'))
+				: localStorage.clear();
+
+		if (!User) navigate('/login');
+	}, [navigate]);
+
 	return (
 		<Routes>
 			<Route path="/login" element={<Login />} />
